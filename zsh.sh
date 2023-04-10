867ba8130fa01fed8432fd94fc22592a2f3d0e2a
u4souza <us4code@gmail.com>
2023-04-10 19:08:10 -0300

    Configuração do prompt zsh

---
+++ zsh.sh
@@ -0,0 +1,31 @@
+# Instalar and conf ZSH
+sudo apt install zsh -y
+chsh -s /bin/zsh
+zsh
+
+# Instalar Oh-my-zsh! -> https://ohmyz.sh/
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Instalar Spaceship Prompt
+# https://github.com/spaceship-prompt/spaceship-prompt
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Mudar ~/.zshrc -> ZSH_THEME="spaceship"
+
+# Instalar Zsh Autosuggestions
+# https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Instalar Zsh Syntax Highlighting
+# https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Mudar plugins
+# nano ~/.zshrc
+# plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+
+# Font optional (https://github.com/pdf/ubuntu-mono-powerline-ttf)
+mkdir -p ~/.fonts
+git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
+fc-cache -vf
